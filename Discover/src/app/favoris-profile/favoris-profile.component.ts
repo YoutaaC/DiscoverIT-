48448599fc49f3ff9a58ba99af220852af59a3d8
YoutaaC <154541214+YoutaaC@users.noreply.github.com>
2024-05-09 19:47:12 +0100

    mycode

--- Discover/src/app/favoris-profile/favoris-profile.component.ts
+++ Discover/src/app/favoris-profile/favoris-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favoris-profile',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./favoris-profile.component.css']
 })
 export class FavorisProfileComponent {
-
+  constructor(public router: Router){}
+  logout(){
+    localStorage.removeItem("user");
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("accessToken");
+    window.location.reload();
+    this.router.navigate(["/"])
+  }
 }
--- Discover/src/app/historique-reservation/historique-reservation.component.ts
+++ Discover/src/app/historique-reservation/historique-reservation.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { EventService } from '../event.service';
+import { Router } from '@angular/router';
+import { VisiteurService } from '../visiteur.service';
+import { User } from '../models/user.model';
+import { Event } from '../models/event.model';
+
 
 @Component({
   selector: 'app-historique-reservation',
@@ -6,5 +12,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./historique-reservation.component.css']
 })
 export class HistoriqueReservationComponent {
+  events: Event[] = [];
+  accessToken!: any;
+  currentUser!: User;
+
+  ngOnInit(): void {
+    const userAccessToken = localStorage.getItem("accessToken"); 
+
+    const userData = localStorage.getItem("accessToken");
+       if (userData) {
+      this.currentUser = JSON.parse(userData);
+
+      this.accessToken = userAccessToken;
+    } else {
+      console.error('User data not found in local storage');
+    }
+    this.getAllEvents();
+
+  }
+  constructor(private eventService: EventService, private router: Router,private visiteurService: VisiteurService) {}
+
+  getAllEvents(): void {
+    this.visiteurService.getAllEvents().subscribe(evnt => {
+      this.events = evnt;
+    }, error => {
+      console.error('Error fetching events:', error);
+    });
+  }
+
+  isOpen = false; // Flag to track dropdown visibility
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  logout(){
+    localStorage.removeItem("user");
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("accessToken");
+    window.location.reload();
+    this.router.navigate(["/"])
+  }
 
 }
