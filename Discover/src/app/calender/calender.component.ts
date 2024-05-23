5907246a9b9161de459f506e55d5bfa5aa696b78
YoutaaC <154541214+YoutaaC@users.noreply.github.com>
2024-05-23 22:28:13 +0100

    mycode

--- Discover/src/app/calender/calender.component.ts
+++ Discover/src/app/calender/calender.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 declare var createGoogleEvent: any;
-
-
+declare const gapi: any; 
+declare var google: any;
 
 
 @Component({
@@ -11,34 +11,130 @@ declare var createGoogleEvent: any;
   styleUrls: ['./calender.component.css']
 })
 export class CalenderComponent implements OnInit{
-constructor(private fb:FormBuilder){}
-appointmentForm!:FormGroup;
- ngOnInit(): void {
-   this.appointmentForm= this.fb.group({
-    appointmentTime: ['',[Validators.required]],
-    email: ['',[Validators.required,Validators.email]],
-   });
- }
+  appointmentForm!: FormGroup;
 
- schedueleMeeting( formData: any){
-  // const eventDetails=this.appointmentForm.value
-  // console.log(eventDetails);
-  // createGoogleEvent(formData);
-  let appointmentTime = new Date(this.appointmentForm.value.appointmentTime)
+  CLIENT_ID = "1091432199733-svgrbjbecdgj2k86rhoas9s5mpjoio4d.apps.googleusercontent.com";
+  API_KEY = "AIzaSyBJV5uyPVrIU0gtDCwAv1dg0lvKSJ9ln7g";
+  DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+  SCOPES = "https://www.googleapis.com/auth/calendar.events";
+  tokenClient: any;
+  gapiInited = false;
+  gisInited = false;
 
-const startTime = appointmentTime. toISOString().slice(0, 18) + '-01:00';
-const endTime = this. getEndTime(appointmentTime) ;
-const eventDetails = {
-email: this.appointmentForm.value.email,
-startTime: startTime,
-endTime:endTime,
- };
- console.info(eventDetails);
-}
+  constructor(private fb: FormBuilder) {}
 
- getEndTime(appointmentTime: Date){
-  appointmentTime.setHours(appointmentTime.getHours() + 1);
-  const endTime =appointmentTime.toISOString().slice(0,18)+'-01:00';
- }
- 
+  ngOnInit(): void {
+    this.appointmentForm = this.fb.group({
+      appointmentTime: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+    this.loadGapi();
+    this.loadGis();
+  }
+
+  loadGapi() {
+    const script = document.createElement('script');
+    script.src = "https://apis.google.com/js/api.js";
+    script.onload = () => this.gapiLoaded();
+    document.body.appendChild(script);
+  }
+
+  gapiLoaded() {
+    gapi.load("client", this.initializeGapiClient.bind(this));
+  }
+
+  async initializeGapiClient() {
+    await gapi.client.init({
+      apiKey: this.API_KEY,
+      discoveryDocs: [this.DISCOVERY_DOC],
+    });
+    this.gapiInited = true;
+  }
+
+  loadGis() {
+    const script = document.createElement('script');
+    script.src = "https://accounts.google.com/gsi/client";
+    script.onload = () => this.gisLoaded();
+    document.body.appendChild(script);
+  }
+
+  gisLoaded() {
+    this.tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: this.CLIENT_ID,
+      scope: this.SCOPES,
+      callback: '',
+    });
+    this.gisInited = true;
+  }
+
+  scheduleMeeting(formData: any) {
+    if (this.gapiInited && this.gisInited) {
+      let appointmentTime = new Date(this.appointmentForm.value.appointmentTime);
+      const startTime = appointmentTime.toISOString();
+      const endTime = this.getEndTime(appointmentTime);
+      const eventDetails = {
+        email: this.appointmentForm.value.email,
+        startTime: startTime,
+        endTime: endTime,
+      };
+      console.info(eventDetails);
+      this.createGoogleEvent(eventDetails);
+    } else {
+      console.error('Google API client not initialized.');
+    }
+  }
+
+  getEndTime(appointmentTime: Date): string {
+    const endTime = new Date(appointmentTime);
+    endTime.setHours(endTime.getHours() + 1);
+    return endTime.toISOString();
+  }
+
+  createGoogleEvent(eventDetails: any) {
+    this.tokenClient.callback = async (resp: any) => {
+      if (resp.error !== undefined) {
+        throw resp;
+      }
+      await this.scheduleEvent(eventDetails);
+    };
+
+    if (gapi.client.getToken() === null) {
+      this.tokenClient.requestAccessToken({ prompt: "consent" });
+    } else {
+      this.tokenClient.requestAccessToken({ prompt: "" });
+    }
+  }
+
+  async scheduleEvent(eventDetails: any) {
+    const event = {
+      summary: "Scheduled Meeting",
+      location: "Online",
+      description: "Scheduled via Google Calendar API",
+      start: {
+        dateTime: eventDetails.startTime,
+        timeZone: "UTC",
+      },
+      end: {
+        dateTime: eventDetails.endTime,
+        timeZone: "UTC",
+      },
+      attendees: [{ email: eventDetails.email }],
+      reminders: {
+        useDefault: false,
+        overrides: [
+          { method: "email", minutes: 24 * 60 },
+          { method: "popup", minutes: 10 },
+        ],
+      },
+    };
+
+    const request = gapi.client.calendar.events.insert({
+      calendarId: "primary",
+      resource: event,
+    });
+
+    request.execute(function (event: any) {
+      console.info("Event created: " + event.htmlLink);
+    });
+  }
 }
--- Discover/src/assets/scripts/googleEvent.js
+++ Discover/src/assets/scripts/googleEvent.js
@@ -1,71 +1,78 @@
 const CLIENT_ID = "1091432199733-svgrbjbecdgj2k86rhoas9s5mpjoio4d.apps.googleusercontent.com";
 const API_KEY = "AIzaSyBJV5uyPVrIU0gtDCwAv1dg0lvKSJ9ln7g";
-const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calender/v3/rest";
-const SCOPES = "https://www.googleapis.com/auth/calender";
+const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+const SCOPES = "https://www.googleapis.com/auth/calendar";
+
 let tokenClient;
-let gapiInited =false;
-let gisIninted =false;
+let gapiInited = false;
+let gisInited = false;
 
-function gapiLoaded(){
-gapi.load("client", initializeGapiClient);
+function gapiLoaded() {
+  gapi.load("client", initializeGapiClient);
 }
 
-async function initializeGapiClient(){
-    await gapi.client.init({
-        apiKey: API_KEY,
-        discoveryDocs: [DISCOVERY_DOC],
-    });
-    gapiInited = true;
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
 }
 
-function gisLoaded(){
-    tokenClient =google.accounts.oauth2.initTokenClient({
-          client_id:CLIENT_ID,
-          scope:SCOPES,
-          callback:"",
-    });
-    gisIninted=true;
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: "", // will be set later
+  });
+  gisInited = true;
 }
 
 function createGoogleEvent(eventDetails) {
-  
-   tokenClient.callback = async (resp) => {
-        if(resp.error!== undefined){
-            throw resp;
-        }
-        await scheduleEvent(eventDetails);
-        if(gapi.client.getToken() === null){
-            tokenClient.requestAccessToken({ prompt: "consent"});
-        }else{
-            tokenClient.requestAccessToken({ prompt: ""});
-        }
-   };
-  }
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw resp;
+    }
+    await scheduleEvent(eventDetails);
+  };
 
-function scheduleEvent(eventDetails){
-const event={
-    summary:"Google I/0 2015",
-    location: "Tunis",
-    description:"aa",
-    start:{
-    dateTime: eventDetails.startTime,
-    timeZone:"tunis/tunis",
+  if (gapi.client.getToken() === null) {
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+async function scheduleEvent(eventDetails) {
+  const event = {
+    summary: "Google I/O 2015",
+    location: "Manzah 6",
+    description: "Event Alert",
+    start: {
+      dateTime: eventDetails.startTime,
+      timeZone: "Africa/Tunis",
+    },
+    end: {
+      dateTime: eventDetails.endTime, // Assuming eventDetails contains an endTime
+      timeZone: "Africa/Tunis",
     },
     recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
-    attendees: [{email :eventDetails.email}],
-    remiders:{
-        userDefault: false,
-        overrides:[
-        {method : "email" , minutes: 24*60},
-        {method : "popup", minutes: 10},
-    ],
+    attendees: [{ email: eventDetails.email }],
+    reminders: {
+      useDefault: false,
+      overrides: [
+        { method: "email", minutes: 24 * 60 },
+        { method: "popup", minutes: 10 },
+      ],
     },
-};
-const request = gapi.client.calender.events.insert({
-    calenderId:"primary",
+  };
+
+  const request = gapi.client.calendar.events.insert({
+    calendarId: "primary",
     resource: event,
-});
-request.execute(function(event){
-    console.info("Event created:" + event.htmlLink);
-})
+  });
+
+  request.execute((event) => {
+    console.info("Event created: " + event.htmlLink);
+  });
 }
