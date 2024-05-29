22d2dd2ec87867a11032ccfaf21cf9d997610101
YoutaaC <154541214+YoutaaC@users.noreply.github.com>
2024-05-29 23:11:15 +0100

    mycode

--- Discover/src/app/evenement/evenement.component.ts
+++ Discover/src/app/evenement/evenement.component.ts
@@ -15,93 +15,108 @@ import { Categorie } from '../models/categorie.model';
 export class EvenementComponent {
 
   events: Event[] = [];
-  catgs:Categorie[] =[];
+  catgs: Categorie[] = [];
   accessToken!: any;
   currentUser!: User;
-
-  typeFilter:string[] = [];
-
+ 
+  typeFilter: string[] = [];
+  pricesChecked: number[] = [];
+ 
   ngOnInit(): void {
-    const userAccessToken = localStorage.getItem("accessToken"); 
-
-    const userData = localStorage.getItem("accessToken");
-       if (userData) {
+    const userAccessToken = localStorage.getItem('accessToken');
+ 
+    const userData = localStorage.getItem('accessToken');
+    if (userData) {
       this.currentUser = JSON.parse(userData);
-
+ 
       this.accessToken = userAccessToken;
     } else {
       console.error('User data not found in local storage');
     }
     this.getAllEvents();
     this.getAllCategories();
-
   }
-  constructor(private eventService: EventService, private router: Router,private mailService: MailService,private categorieService:CategorieService) {}
-
+  constructor(
+    private eventService: EventService,
+    private router: Router,
+    private mailService: MailService,
+    private categorieService: CategorieService
+  ) {}
+ 
   getAllEvents(): void {
-    this.eventService.getAllEvents().subscribe(evtns => {
-      this.events = evtns;
-      console.log(evtns);
-    }, error => {
-      console.error('Error fetching events:', error);
-    });
+    this.eventService.getAllEvents().subscribe(
+      (evtns) => {
+        this.events = evtns;
+        console.log(evtns);
+      },
+      (error) => {
+        console.error('Error fetching events:', error);
+      }
+    );
   }
-
-
+ 
   getAllCategories(): void {
-    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
-    this.categorieService.getAllCategories().subscribe(evtns => {
-      this.catgs = evtns;
-      
-    }, error => {
-      console.error('Error fetching events:', error);
-    });
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+ 
+    this.categorieService.getAllCategories().subscribe(
+      (evtns) => {
+        this.catgs = evtns;
+      },
+      (error) => {
+        console.error('Error fetching events:', error);
+      }
+    );
   }
-
-  isOpen = false; 
-
+ 
+  isOpen = false;
+ 
   toggleDropdown() {
     this.isOpen = !this.isOpen;
   }
-
-  gotoReservation(id :number) {
-    this.router.navigate(["/reservation",id])
+ 
+  gotoReservation(id: number) {
+    this.router.navigate(['/reservation', id]);
   }
-
-  gotoDetailsEvent(id :number) {
-    this.router.navigate(["/detailsEvent",id])
+ 
+  gotoDetailsEvent(id: number) {
+    this.router.navigate(['/detailsEvent', id]);
   }
-
-  calender(id :number) {
-    this.router.navigate(["/calender",id])
+ 
+  calender(id: number) {
+    this.router.navigate(['/calender', id]);
   }
-
-
+ 
   filter(events: Event[]): Event[] {
-    if(this.typeFilter.length===0){
-      return events;
-    }
-    console.log("typeFilter",this.typeFilter)
-    events.forEach((e)=>{
-      console.log(e.type,this.typeFilter.includes(e.type))
-    })
-    let result: Event[] = events.filter((e)=> this.typeFilter.includes(e.type) )      
+    const FilterByType= this.typeFilter.length > 0;
+    const FilterByPrice= this.pricesChecked.length > 0 ? this.pricesChecked.reduce((a, b) => Math.min(a, b), 2) : 0;
+    console.log('pricesChecked', this.pricesChecked);
+    events.forEach((e) => {
+      console.log(e.prix,(FilterByPrice <= e.prix ),FilterByType);
+    });
+ 
+    let result: Event[] = events.filter((e) =>
+      (this.typeFilter.includes(e.type) || FilterByType) &&
+      (FilterByPrice <= e.prix )
+    );
     console.log(result);
     return result;
-    
   }
-
-   getCheck(chkbox:string){
-      
-      if(this.typeFilter.includes(chkbox)){
-        this.typeFilter.splice(this.typeFilter.indexOf(chkbox),1);
-        
-      }else{
-        this.typeFilter.push(chkbox);
-      }
-     
+ 
+  getCheck(chkbox: string) {
+    if (this.typeFilter.includes(chkbox)) {
+      this.typeFilter.splice(this.typeFilter.indexOf(chkbox), 1);
+    } else {
+      this.typeFilter.push(chkbox);
     }
+  }
+ 
+  handlePriceChecked(price: number) {
+    if (this.pricesChecked.includes(price)) {
+      this.pricesChecked.splice(this.pricesChecked.indexOf(price), 1);
+    } else {
+      this.pricesChecked.push(price);
+    }
+  }
   }
 
   
