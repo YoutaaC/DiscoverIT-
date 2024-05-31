bd7a2f25f9f9e8d0136e52ff73245d797d4d12dc
YoutaaC <154541214+YoutaaC@users.noreply.github.com>
2024-05-31 22:38:28 +0100

    mycode

--- Discover/src/app/about-visiteur/about-visiteur.component.ts
+++ Discover/src/app/about-visiteur/about-visiteur.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-about-visiteur',
   templateUrl: './about-visiteur.component.html',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AboutVisiteurComponent {
 
+  constructor( private router:Router) {}
+
+  wannasignup(){
+    Swal.fire({
+      title: "Créer un compte",
+      text: "Cette fonctionnalité est accessible lorsque vous créez un compte.",
+      icon: "info",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "S'inscrire"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigate(['signup']);
+      }
+    });
+  }
+
 }
+
+
+
